e91007e1e1f9a99f1ea3adaa2240ce60365e1e68
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-20 22:52:09 +0530

    day-1

--- Backend/Models/Users.cs
+++ Backend/Models/Users.cs
@@ -2,6 +2,13 @@ namespace EMedicine.Models
 {
     public class Users
     {
-        
+        public int UserId {get;set;}
+        public string Name {get;set;}
+        public string Email {get;set;}
+        public string Password {get;set;}
+        public string Address {get;set;}
+        public string Type {get;set;}
+        public int Status {get;set;}
+        public DateTime CreatedON {get;set;}
     }
 }
