bc3468086151576669e7be1e8247e64251865f17
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-21 11:38:53 +0530

    day-2

--- Backend/Models/Response.cs
+++ Backend/Models/Response.cs
@@ -4,5 +4,7 @@
     {
         public int StatusCode {get;set;}
         public string StatusMessage {get;set;}
+        public List<Users> listUsers {get;set;}
+        public Users user {get;set;}
     }
 }
