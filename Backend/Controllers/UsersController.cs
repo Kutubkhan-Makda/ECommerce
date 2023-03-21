c856438b9a3c8048ef095459e4dc7cb294777f36
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-21 12:13:15 +0530

    day-2

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMedicine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        
+    }
+}
