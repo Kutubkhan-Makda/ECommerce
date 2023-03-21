ecc50be6130652ef10012dc08d9db89305ac657e
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-21 12:01:26 +0530

    day-2

---
+++ Backend/Models/Orders.cs
@@ -0,0 +1,13 @@
+namespace EMedicine.Models
+{
+    public class Orders
+    {
+        public int OrderId {get;set;}
+        public int UserId {get;set;}
+        public String OrderNo {get;set;}
+        public Decimal TotalPrice {get;set;}
+        public String ShippingAddress {get;set;}
+        public DateTime OrderDate {get;set;}
+        public String OrderStatus {get;set;}
+    }
+}
