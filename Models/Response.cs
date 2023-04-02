3331b639b655f7c2a7868e9f48705d0fa30785d9
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-04-02 17:07:27 +0530

    day-4

--- Models/Response.cs
+++ Models/Response.cs
@@ -2,17 +2,17 @@
 {
     public class Response
     {
-        public int StatusCode {get;set;}
-        public String StatusMessage {get;set;}
-        public List<Users> listUsers {get;set;}
-        public Users user {get;set;}
-        public List<Products> listProducts {get;set;}
-        public Products product {get;set;}
-        public List<Cart> listCart {get;set;}
-        public List<Orders> listOrders {get;set;}
-        public Orders order {get;set;}
-        public List<OrderItems> listOrderItems {get;set;}
-        public OrderItems orderItem {get;set;}
+        //public int StatusCode {get;set;}
+        //public String StatusMessage {get;set;}
+        //public List<Users> listUsers {get;set;}
+        //public Users user {get;set;}
+        //public List<Products> listProducts {get;set;}
+        //public Products product {get;set;}
+        //public List<Cart> listCart {get;set;}
+        //public List<Orders> listOrders {get;set;}
+        //public Orders order {get;set;}
+        //public List<OrderItems> listOrderItems {get;set;}
+        //public OrderItems orderItem {get;set;}
 
     }
 }
