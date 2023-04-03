15786d263f6fc4c81522ec5f8003ae48c90ddce4
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-04-03 23:28:46 +0530

    day-5

--- Areas/Products/Controllers/ProductsController.cs
+++ Areas/Products/Controllers/ProductsController.cs
@@ -17,6 +17,50 @@ namespace ECommerce.Areas.Products.Controllers
             return View("ProductsList",dtProduct);
         }
 
+        public IActionResult Add(int? ProductId)
+         {
+            //DataTable dt1 = ProductsDAL.PR_LOC_Country_SelectByDropdownList();
+            //
+            //List<Areas.Models.LOC_CountryDropDownModel> list=new List<Areas.Models.LOC_CountryDropDownModel>();
+            //foreach(DataRow dr in dt1.Rows)
+            //{
+            //    Areas.Models.LOC_CountryDropDownModel modelLOC_CountryDropDown = new Areas.Models.LOC_CountryDropDownModel();
+            //    modelLOC_CountryDropDown.CountryID = (Convert.ToInt32(dr["CountryID"]));
+            //    modelLOC_CountryDropDown.CountryName = (Convert.ToString(dr["CountryName"]));
+            //    list.Add(modelLOC_CountryDropDown);
+            //}
+            //ViewBag.CountryList=list;
+            if (ProductId != null)
+            {
+                //String str = this.Configuration.GetConnectionString("SQL_AddressBook");
+                //SqlConnection conn = new SqlConnection(str);
+                //conn.Open();
+                //SqlCommand cmd = conn.CreateCommand();
+                //cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.CommandText = "PR_LOC_State_SelectByPK";
+                //cmd.Parameters.Add("@StateID", SqlDbType.Int).Value = StateID;
+                DataTable dt = ProductsDAL.PR_Product_SelectbyPK(ProductId);
+                //SqlDataReader objSDR = cmd.ExecuteReader();
+                //dt.Load(objSDR);
+                if(dt.Rows.Count > 0)
+                {
+                    Products productsModel = new Products();
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        productsModel.StateID = (Convert.ToInt32(dr["StateID"]));
+                        productsModel.StateName = (Convert.ToString(dr["StateName"]));
+                        productsModel.StateCode = (Convert.ToString(dr["StateCode"]));
+                        productsModel.CreationDate = (Convert.ToDateTime(dr["CreationDate"]));
+                        productsModel.ModificationDate = (Convert.ToDateTime(dr["ModificationDate"]));
+                        productsModel.CountryID = (Convert.ToInt32(dr["CountryID"]));
+                    }
+
+                    return View("ProductsAddEdit", productsModel);
+                } 
+            }
+            return View("ProductsAddEdit");
+        }
+
         // GET: ProductsController/Details/5
         public ActionResult Details(int id)
         {
