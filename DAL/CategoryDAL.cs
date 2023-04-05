f3065f4191931e126153bfa0e350867bb8e5707a
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-04-05 23:10:40 +0530

    day-6

--- DAL/CategoryDAL.cs
+++ DAL/CategoryDAL.cs
@@ -27,5 +27,69 @@ namespace ECommerce.DAL
                 return null;
             }
         }
+
+        public bool? PR_Category_Delete(int? CategoryId)
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Category_DeleteByPK");
+                sqlDB.AddInParameter(dbCMD, "CategoryId", SqlDbType.Int, CategoryId);
+                int vReturnValue = sqlDB.ExecuteNonQuery(dbCMD);
+                return (vReturnValue == -1 ? false : true);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable PR_Category_SelectbyPK(int? CategoryId)
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Manufacturer_SelectByPK");
+                sqlDB.AddInParameter(dbCMD, "CategoryId", SqlDbType.Int, CategoryId);
+
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool? PR_Category_Save(int? CategoryId,string? CategoryName)
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD;
+                if(CategoryId == null)
+                {
+                    dbCMD = sqlDB.GetStoredProcCommand("PR_Category_Insert");
+                }
+                else
+                {
+                    dbCMD = sqlDB.GetStoredProcCommand("PR_Category_UpdateByPK");
+                    sqlDB.AddInParameter(dbCMD, "@CategoryId",SqlDbType.Int, CategoryId);
+                }
+                sqlDB.AddInParameter(dbCMD, "@CategoryName",SqlDbType.VarChar, CategoryName);
+
+                int vReturnValue = sqlDB.ExecuteNonQuery(dbCMD);
+                return (vReturnValue == -1 ? false : true);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
