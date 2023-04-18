59cd3c19845a42180edb5876e6280be3f0ed0699
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-04-18 22:26:59 +0530

    day-15

--- Areas/Category/Controllers/CategoryController.cs
+++ Areas/Category/Controllers/CategoryController.cs
@@ -17,55 +17,26 @@ namespace ECommerce.Areas.Category.Controllers
             return View("CategoryListAdmin",dtCategory);
         }
 
-        // GET: CategoryController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Add(int? CategoryId)
         {
-            return View();
-        }
-
-        // GET: CategoryController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CategoryController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            if (CategoryId != null)
             {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+                DataTable dt = categoryDAL.PR_Category_SelectbyPK(CategoryId);
+                if(dt.Rows.Count > 0)
+                {
+                    Areas.Category.Models.Category categoryModel = new Areas.Category.Models.Category();
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        categoryModel.CategoryId = (Convert.ToInt32(dr["CategoryId"]));
+                        categoryModel.CategoryName = (Convert.ToString(dr["CategoryName"]));
+                    }
 
-        // GET: CategoryController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: CategoryController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
+                    return View("CategoryAddEdit", categoryModel);
+                } 
             }
-            catch
-            {
-                return View();
-            }
+            return View("CategoryAddEdit");
         }
-
-        // GET: CategoryController/Delete/5
+        
         public ActionResult Delete(int id)
         {
             return View();
