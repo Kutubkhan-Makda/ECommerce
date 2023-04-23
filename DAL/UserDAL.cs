54f4a0b1ec351758d5f3b75bc12a39354716ed19
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-04-23 22:32:03 +0530

    day-18

--- DAL/UserDAL.cs
+++ DAL/UserDAL.cs
@@ -12,7 +12,7 @@ namespace ECommerce.DAL
             try
             {
                 SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
-                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Category_SelectAll");
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Users_SelectAll");
 
                 DataTable dt = new DataTable();
                 using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
