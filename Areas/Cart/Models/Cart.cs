41cd2317e0ec96bd909e66482048e74bfb2592ee
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-04-27 20:50:19 +0530

    day-22

--- Areas/Cart/Models/Cart.cs
+++ Areas/Cart/Models/Cart.cs
@@ -2,7 +2,7 @@
 {
     public class Cart
     {
-        public int CartId { get; set; }
+        public int? CartId { get; set; }
         public int UserId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
