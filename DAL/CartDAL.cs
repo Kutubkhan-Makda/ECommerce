ab5d0838a6ad7b3487396c6c0fd02021a0d79d3d
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-04-29 18:44:39 +0530

    day-24

---
+++ DAL/CartDAL.cs
@@ -0,0 +1,52 @@
+using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+using ECommerce.Auth;
+
+namespace ECommerce.DAL
+{
+    public class CartDAL:DALConnection
+    {
+        public DataTable PR_Cart_SelectAll()
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Cart_SelectAll");
+                sqlDB.AddInParameter(dbCMD, "UserId", SqlDbType.Int, @CV.UserID());
+
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool? PR_Cart_InsertItem(int? ProductId,int? Quantity,decimal? TotalPrice)
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Cart_Insert");
+                sqlDB.AddInParameter(dbCMD, "@ProductId",SqlDbType.Int, ProductId);
+                sqlDB.AddInParameter(dbCMD, "@Quantity",SqlDbType.Int, Quantity);
+                sqlDB.AddInParameter(dbCMD, "@TotalPrice",SqlDbType.Decimal, TotalPrice);
+
+                int vReturnValue = sqlDB.ExecuteNonQuery(dbCMD);
+                return (vReturnValue == -1 ? false : true);
+            }
+            catch (Exception ex)
+            {
+                return null;    
+            }
+        }
+    }
+}
