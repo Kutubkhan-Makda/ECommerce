8767fedb4bd048d08ef857967814c186b8303cde
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-04-30 21:44:16 +0530

    day-25

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace ECommerce.Controllers
             return View("../Users/Home",dtCart);
         }
 
+        public IActionResult About()
+        {
+            return View("../Users/About");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
