47cf31a2429864881d5f144093f3f137828101b8
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-05-01 15:09:29 +0530

    day-26

--- DAL/Sales.cs
+++ DAL/Sales.cs
@@ -7,6 +7,41 @@ namespace ECommerce.DAL
 {
     public class SalesDAL:DALConnection
     {
-        
+        public DataTable PR_Sales_SelectAll()
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Cart_SelectbyUser");
+
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool? PR_Sales_Delete(int? SalesId)
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Product_DeleteByPK");
+                sqlDB.AddInParameter(dbCMD, "SalesId", SqlDbType.Int, SalesId);
+                int vReturnValue = sqlDB.ExecuteNonQuery(dbCMD);
+                return (vReturnValue == -1 ? false : true);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
