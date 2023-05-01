6d32c1ce2c00b8db1d5c05a1e03c3b926a61adae
Makda Kutubkhan <kutubkhanmakda1792@gmail.com>
2023-05-01 15:02:09 +0530

    day-26

---
+++ DAL/Sales.cs
@@ -0,0 +1,12 @@
+using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ECommerce.DAL
+{
+    public class SalesDAL:DALConnection
+    {
+        
+    }
+}
