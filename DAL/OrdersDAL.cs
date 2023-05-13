464d5661c954ef73d3aae86c37b3240df3321835
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-13 16:00:16 +0530

    day-38

---
+++ DAL/OrdersDAL.cs
@@ -0,0 +1,10 @@
+using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+using ECommerce.Auth;
+
+namespace ECommerce.DAL
+{
+    
+}
