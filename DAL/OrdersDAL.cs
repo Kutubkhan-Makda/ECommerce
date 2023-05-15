b89bb6331b18a917cb7823931ab541be328f4c56
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-16 01:49:09 +0530

    day-41

--- DAL/OrdersDAL.cs
+++ DAL/OrdersDAL.cs
@@ -8,27 +8,28 @@ namespace ECommerce.DAL
 {
     public class OrdersDAL:DALConnection
     {
-        //public DataTable PR_Cart_SelectbyUser(int? UserId)
-        //{
-        //    try
-        //    {
-        //        SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
-        //        DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Cart_SelectbyUser");
-        //        sqlDB.AddInParameter(dbCMD, "UserId", SqlDbType.Int, UserId);
-//
-        //        DataTable dt = new DataTable();
-        //        using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
-        //        {
-        //            dt.Load(dr);
-        //        }
-        //        return dt;
-//
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return null;
-        //    }
-        //}
+        public DataTable PR_Order_SelectLatest()
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_Order_SelectLatest");
+                sqlDB.AddInParameter(dbCMD, "@UserId", SqlDbType.Int, CV.UserId());
+                sqlDB.AddInParameter(dbCMD, "@OrderId",SqlDbType.Int, DBNull.Value);
+
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
 
         public bool? PR_Orders_Insert(int? OrderId,string? ShippingAddress)
         {
