753e14a2d05baa3ea1bb876097264f043fee6aec
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-23 01:05:43 +0530

    day-51

--- Areas/Users/Models/Users.cs
+++ Areas/Users/Models/Users.cs
@@ -15,10 +15,4 @@
         public String ImageUrl { get; set; }
         public DateTime CreatedON { get; set; }
     }
-
-    public class CountryDropdown
-    {
-        public int? CountryID { get; set; }
-        public int CountryName { get; set; }
-    }
 }
