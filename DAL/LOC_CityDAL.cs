917cd304f079b8c3d6988f7cf423a738f0c23e44
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-24 20:22:52 +0530

    day-52

--- DAL/LOC_CityDAL.cs
+++ DAL/LOC_CityDAL.cs
@@ -1,6 +1,11 @@
-﻿namespace ECommerce.DAL
+﻿using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+namespace ECommerce.DAL
 {
     public class LOC_CityDAL : DALConnection
     {
+
     }
 }
--- DAL/LOC_CountryDAL.cs
+++ DAL/LOC_CountryDAL.cs
@@ -1,7 +1,31 @@
-﻿namespace ECommerce.DAL
+﻿using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+namespace ECommerce.DAL
 {
     public class LOC_CountryDAL : DALConnection
     {
+        public DataTable PR_LOC_Country_SelectAll()
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_Country_SelectAll");
 
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
     }
 }
