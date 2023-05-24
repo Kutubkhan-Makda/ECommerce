b742a1b2fdc79e43f4aef3cdb554d0bf303a3e78
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-24 20:43:25 +0530

    day-52

--- DAL/LOC_StateDAL.cs
+++ DAL/LOC_StateDAL.cs
@@ -12,7 +12,6 @@ namespace ECommerce.DAL
             {
                 SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
                 DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_SelectAll");
-                sqlDB.AddInParameter(dbCMD, "UserID", SqlDbType.Int, @CV.UserID());
 
                 DataTable dt = new DataTable();
                 using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
