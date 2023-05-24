583ccdbcca492115145ea77f16371c32dede0e86
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-24 20:44:33 +0530

    day-52

--- DAL/LOC_CityDAL.cs
+++ DAL/LOC_CityDAL.cs
@@ -6,6 +6,27 @@ namespace ECommerce.DAL
 {
     public class LOC_CityDAL : DALConnection
     {
+        public DataTable PR_LOC_City_SelectAll()
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_City_SelectAll");
+                sqlDB.AddInParameter(dbCMD, "UserID", SqlDbType.Int, @CV.UserID());
 
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
     }
 }
