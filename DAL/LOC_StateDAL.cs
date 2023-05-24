8a72024888d5b71757cffd29b46e0634d7fbe8e1
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-24 20:43:03 +0530

    day-52

--- DAL/LOC_StateDAL.cs
+++ DAL/LOC_StateDAL.cs
@@ -1,6 +1,29 @@
-﻿namespace ECommerce.DAL
+﻿
+namespace ECommerce.DAL
 {
     public class LOC_StateDAL : DALConnection
     {
+        public DataTable PR_LOC_State_SelectAll()
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_SelectAll");
+                sqlDB.AddInParameter(dbCMD, "UserID", SqlDbType.Int, @CV.UserID());
+
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
     }
 }
