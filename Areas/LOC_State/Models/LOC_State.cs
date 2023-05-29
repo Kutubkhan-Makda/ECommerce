460bc2998a2318b40e2704eddbfd4ad3069dc88f
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-05-29 10:10:01 +0530

    day-56

--- Areas/LOC_State/Models/LOC_State.cs
+++ Areas/LOC_State/Models/LOC_State.cs
@@ -2,7 +2,9 @@
 {
     public class LOC_State
     {
-        public int StateId { get; set; }
+        public int? StateId { get; set; }
         public string StateName { get; set; }
+        public string StateCode { get; set; }
+        public int CountryId { get; set; }
     }
 }
