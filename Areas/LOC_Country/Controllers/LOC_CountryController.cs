997ca217da38e86ef322299ad27247fa1d0cc68b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-06-13 15:02:25 +0530

    day-64

--- Areas/LOC_Country/Controllers/LOC_CountryController.cs
+++ Areas/LOC_Country/Controllers/LOC_CountryController.cs
@@ -25,16 +25,14 @@ namespace ECommerce.Areas.LOC_Country.Controllers
         {
             if (CountryId != null)
             {
-                DataTable dt = countryDAL.(CountryId);
+                DataTable dt = countryDAL.PR_LOC_Country_SelectByPK(CountryId);
 
-                LOC_CountryModel modelLOC_Country = new LOC_CountryModel();
+                Areas.LOC_Country.Models.LOC_Country modelLOC_Country = new Areas.LOC_Country.Models.LOC_Country();
                 foreach (DataRow dr in dt.Rows)
                 {
-                    modelLOC_Country.CountryID = (Convert.ToInt32(dr["CountryID"]));
+                    modelLOC_Country.CountryId = (Convert.ToInt32(dr["CountryId"]));
                     modelLOC_Country.CountryName = (Convert.ToString (dr["CountryName"]));
                     modelLOC_Country.CountryCode = (Convert.ToString (dr["CountryCode"]));
-                    modelLOC_Country.CreationDate = (Convert.ToDateTime(dr["CreationDate"]));
-                    modelLOC_Country.ModificationDate = (Convert.ToDateTime(dr["ModificationDate"]));
                 }
                 return View("LOC_CountryAddEdit", modelLOC_Country);
             }
@@ -42,11 +40,9 @@ namespace ECommerce.Areas.LOC_Country.Controllers
         }
 
         [HttpPost]
-        public IActionResult Save(LOC_CountryModel modelLoc_Country)
-        {
-            LOC_DAL dalLOC = new LOC_DAL();
-             
-            if(Convert.ToBoolean(dalLOC.PR_LOC_Save_Country(modelLoc_Country.CountryID,modelLoc_Country.CountryName,modelLoc_Country.CountryCode)))
+        public IActionResult Save(Areas.LOC_Country.Models.LOC_Country  modelLOC_Country)
+        {    
+            if(Convert.ToBoolean(countryDAL.PR_LOC_Save_Country(modelLoc_Country.CountryID,modelLoc_Country.CountryName,modelLoc_Country.CountryCode)))
             {
                 if(modelLoc_Country.CountryID == null)
                 {
