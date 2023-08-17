9a9b7e91cd24fd9044bd2c09be9ea7436c4c3f66
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-17 17:06:38 +0530

    day-66

--- DAL/LOC_StateDAL.cs
+++ DAL/LOC_StateDAL.cs
@@ -49,13 +49,13 @@ namespace ECommerce.DAL
             }
         }
 
-        public bool? PR_LOC_Save_State(int? StateID,int? CountryID,string? StateName,string? StateCode)
+        public bool? PR_LOC_Save_State(int? StateId,int? CountryId,string? StateName,string? StateCode)
         {
             try
             {
                 SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
                 DbCommand dbCMD;
-                if(StateID == null)
+                if(StateId == null)
                 {
                     dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_Insert");
                     sqlDB.AddInParameter(dbCMD, "@CreationDate",SqlDbType.Date, DBNull.Value);
@@ -63,11 +63,11 @@ namespace ECommerce.DAL
                 else
                 {
                     dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_UpdateByPK");
-                    sqlDB.AddInParameter(dbCMD, "@StateID",SqlDbType.Int, StateID);
+                    sqlDB.AddInParameter(dbCMD, "@StateId",SqlDbType.Int, StateId);
                 }
                 sqlDB.AddInParameter(dbCMD, "@StateName",SqlDbType.VarChar, StateName);
                 sqlDB.AddInParameter(dbCMD, "@StateCode",SqlDbType.VarChar, StateCode);
-                sqlDB.AddInParameter(dbCMD, "@CountryID",SqlDbType.Int, CountryID);
+                sqlDB.AddInParameter(dbCMD, "@CountryId",SqlDbType.Int, CountryId);
                 sqlDB.AddInParameter(dbCMD, "@ModificationDate",SqlDbType.Date, DBNull.Value);
                 
 
