345e2032dcd883a1239962425226225bd411e75b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-17 17:03:55 +0530

    day-66

--- DAL/LOC_StateDAL.cs
+++ DAL/LOC_StateDAL.cs
@@ -25,7 +25,28 @@ namespace ECommerce.DAL
             {
                 return null;
             }
+        }
 
+        public DataTable PR_LOC_State_SelectByPK(int? StateID)
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_SelectByPK");
+                sqlDB.AddInParameter(dbCMD, "StateID", SqlDbType.Int, StateID);
+
+                DataTable dt = new DataTable();
+                using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
+                {
+                    dt.Load(dr);
+                }
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public bool? PR_LOC_State_Delete(int? StateId)
