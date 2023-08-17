afadc6668eda794a35868fff1873b90559d087b8
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-17 22:07:03 +0530

    day-66

--- Areas/LOC_State/Controllers/LOC_StateController.cs
+++ Areas/LOC_State/Controllers/LOC_StateController.cs
@@ -12,6 +12,8 @@ namespace ECommerce.Areas.LOC_State.Controllers
     public class LOC_StateController : Controller
     {
         LOC_StateDAL stateDAL = new LOC_StateDAL();
+
+        LOC_CountryDAL lOC_CountryDAL = new LOC_CountryDAL();
         // GET: LOC_StateController
         public ActionResult Admin()
         {
@@ -19,15 +21,49 @@ namespace ECommerce.Areas.LOC_State.Controllers
             return View("StateList",dtState);
         }
 
-        public ActionResult Details(int id)
+        public IActionResult Add(int? StateID)
         {
-            return View();
-        }
+            DataTable dtState = lOC_CountryDAL.PR_LOC_Country_SelectAll();
+            
+            List<Areas.LOC_Country.Models.LOC_Country> countryDropdownlist = new List<Areas.LOC_Country.Models.LOC_Country>();
+            foreach(DataRow dr in dtState.Rows)
+            {
+                Areas.LOC_Country.Models.LOC_Country modelLOC_CountryDropDown = new Areas.LOC_Country.Models.LOC_Country();
+                modelLOC_CountryDropDown.CountryId = (Convert.ToInt32(dr["CountryId"]));
+                modelLOC_CountryDropDown.CountryName = (Convert.ToString(dr["CountryName"]));
+                countryDropdownlist.Add(modelLOC_CountryDropDown);
+            }
+            ViewBag.CountryList = countryDropdownlist;
 
-        // GET: LOC_StateController/Create
-        public ActionResult Create()
-        {
-            return View();
+            if (StateID != null)
+            {
+                //String str = this.Configuration.GetConnectionString("SQL_AddressBook");
+                //SqlConnection conn = new SqlConnection(str);
+                //conn.Open();
+                //SqlCommand cmd = conn.CreateCommand();
+                //cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.CommandText = "PR_LOC_State_SelectByPK";
+                //cmd.Parameters.Add("@StateID", SqlDbType.Int).Value = StateID;
+                DataTable dt = dalLOC.PR_LOC_State_SelectByPK(StateID);
+                //SqlDataReader objSDR = cmd.ExecuteReader();
+                //dt.Load(objSDR);
+                if(dt.Rows.Count > 0)
+                {
+                    LOC_StateModel modelLOC_State = new LOC_StateModel();
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        modelLOC_State.StateID = (Convert.ToInt32(dr["StateID"]));
+                        modelLOC_State.StateName = (Convert.ToString(dr["StateName"]));
+                        modelLOC_State.StateCode = (Convert.ToString(dr["StateCode"]));
+                        modelLOC_State.CreationDate = (Convert.ToDateTime(dr["CreationDate"]));
+                        modelLOC_State.ModificationDate = (Convert.ToDateTime(dr["ModificationDate"]));
+                        modelLOC_State.CountryID = (Convert.ToInt32(dr["CountryID"]));
+                    }
+
+                    return View("LOC_StateAddEdit", modelLOC_State);
+                } 
+            }
+            return View("LOC_StateAddEdit");
         }
 
         // POST: LOC_StateController/Create
