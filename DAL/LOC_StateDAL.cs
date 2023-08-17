2161b989a9b08b12b5f029072d0a62528fa1880f
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-17 17:05:22 +0530

    day-66

--- DAL/LOC_StateDAL.cs
+++ DAL/LOC_StateDAL.cs
@@ -27,13 +27,13 @@ namespace ECommerce.DAL
             }
         }
 
-        public DataTable PR_LOC_State_SelectByPK(int? StateID)
+        public DataTable PR_LOC_State_SelectByPK(int? StateId)
         {
             try
             {
                 SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
                 DbCommand dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_SelectByPK");
-                sqlDB.AddInParameter(dbCMD, "StateID", SqlDbType.Int, StateID);
+                sqlDB.AddInParameter(dbCMD, "StateId", SqlDbType.Int, StateId);
 
                 DataTable dt = new DataTable();
                 using (IDataReader dr = sqlDB.ExecuteReader(dbCMD))
@@ -42,6 +42,37 @@ namespace ECommerce.DAL
                 }
 
                 return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool? PR_LOC_Save_State(int? StateID,int? CountryID,string? StateName,string? StateCode)
+        {
+            try
+            {
+                SqlDatabase sqlDB = new SqlDatabase(SQL_Connection);
+                DbCommand dbCMD;
+                if(StateID == null)
+                {
+                    dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_Insert");
+                    sqlDB.AddInParameter(dbCMD, "@CreationDate",SqlDbType.Date, DBNull.Value);
+                }
+                else
+                {
+                    dbCMD = sqlDB.GetStoredProcCommand("PR_LOC_State_UpdateByPK");
+                    sqlDB.AddInParameter(dbCMD, "@StateID",SqlDbType.Int, StateID);
+                }
+                sqlDB.AddInParameter(dbCMD, "@StateName",SqlDbType.VarChar, StateName);
+                sqlDB.AddInParameter(dbCMD, "@StateCode",SqlDbType.VarChar, StateCode);
+                sqlDB.AddInParameter(dbCMD, "@CountryID",SqlDbType.Int, CountryID);
+                sqlDB.AddInParameter(dbCMD, "@ModificationDate",SqlDbType.Date, DBNull.Value);
+                
+
+                int vReturnValue = sqlDB.ExecuteNonQuery(dbCMD);
+                return (vReturnValue == -1 ? false : true);
             }
             catch (Exception ex)
             {
